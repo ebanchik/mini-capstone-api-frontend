65c40c137c641c19e98bca0f29af331b96871576
ebanchik <ebanchik@gmail.com>
2023-12-21 13:32:58 -0500

    add index action

---
+++ src/ProductsIndex.jsx
@@ -0,0 +1,20 @@
+export function ProductsIndex(props) {
+  return (
+    <div>
+      <h1>All products</h1>
+      {props.products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <img src={product.url} />
+          <p>Width: {product.width}</p>
+          <p>Height: {product.height}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+
+
+
+      
